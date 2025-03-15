1394ccc48e96d84ab0e52c3140a30f56ff6a2de3
Punith L <meghanaggovinda1997@gmail.com>
2025-03-15 17:29:40 +0000

    updated

---
+++ models/marts/student_enrollments.sql
@@ -0,0 +1,38 @@
+with students as (
+    select * from {{ref("stg_students")}}
+    ),
+ enrollments as (
+    select * from {{ref("stg_enrollments")}}
+    ),
+ courses as (
+    select * from {{ref("stg_courses")}}
+    ),
+ final as (
+    select 
+            s.STUDENT_ID,
+            s.FIRST_NAME,
+            s.LAST_NAME,
+            s.EMAIL,
+            s.ENROLLMENT_DATE,
+            s.DATE_OF_BIRTH,
+            s.GENDER,
+            s.AGE,
+            s.ENROLLMENT_YEAR,
+            s.FULL_NAME,
+            c.COURSE_NAME,
+            c.INSTRUCTOR_NAME,
+            c.START_DATE,
+            c.END_DATE,
+            c.CREDITS,
+            c.COUSE_LEVEL,
+            c.COURSE_STATUS,
+            c.COURSE_DURATION_DAYS,
+            e.ENROLLMENT_DATE
+    from 
+    students s
+    left join enrollments e on s.student_id=e.student_id 
+    left join courses c on e.course_id=c.course_id
+ )
+ select *
+ from 
+ final
--- models/staging/stg_courses.sql
+++ models/staging/stg_courses.sql
@@ -1,3 +1,9 @@
 with raw as (select * from {{source("edtech_source","courses")}}), 
      final as (select
-             from raw)
+              r.*,
+              case when credits=4 then 'Advanced' else 'Beginer' end as couse_level,
+              case when end_date<=sysdate()::date then 'INACTIVE' else 'ACTIVE' end as COURSE_STATUS,
+              DATEDIFF(day,start_date,end_date) as course_duration_days
+             from raw r)
+    select *
+    from final
